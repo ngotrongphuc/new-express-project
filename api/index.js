c8c32655d94ec135df8764e0b0149d4b9ceeef4f
ngo trong phuc <phucngo4499@gmail.com>
2024-03-11 21:12:26 +0700

    add countries endpoint

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 
+const countriesData = require('../source/countries.json');
+
 app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.get("/countries", (req, res) => res.send(countriesData));
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
 
