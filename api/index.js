fb7dc142f45fb48612357cf41f444c12a2b93d71
ngo trong phuc <phucngo4499@gmail.com>
2024-03-11 21:35:28 +0700

    add route to access static files

--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 
 const countriesData = require('../source/countries.json');
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.get("/countries", (req, res) => res.send(countriesData));
+
+app.use('/images', express.static(path.join(__dirname, '../public')));
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
 
